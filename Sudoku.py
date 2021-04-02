0f0f6ca7779037f5f59f061f762f5713bd6d4a6f
fjadbeth <74471478+fjadbeth@users.noreply.github.com>
2021-04-02 16:22:56 -0500

    Add files via upload

---
+++ Sudoku.py
@@ -0,0 +1,26 @@
+board = [
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
+]
+
+def solver(board):
+    pass
+
+def isValid(board, num, pos):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            
+
+def isEmpty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)
+    return None
